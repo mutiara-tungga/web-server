8fa8c46da42bfd45982ec88ccfb5ca8f6122d63f
Mutiara <putri@skyshi.com>
2017-05-18 11:14:54 +0700

    Initialize commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function (req,res, next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next){
+		console.log('Request '+ new Date().toString() + ' : ' + req.method+' '+req.originalUrl);
+		next();
+	}
+};
+//route
+// app.get('/', function (req, res) {
+// 	res.send('Hello express');
+// });
+
+// app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+//route
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us');
+})
+
+//get path
+//console.log(__dirname);
+
+//expose folder
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+	console.log('Express start with port : '+port);
+});
+
